0941e0e2e37e0c63020a07d52ff8349bb841ce85
ballotamerica <ec2-user@ip-172-31-59-132.ec2.internal>
2019-02-09 17:51:34 +0000

    Rearrange styling on root path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
    def index
+      if partner_signed_in? && current_partner
+         render partial: "pages/dashboard"
+      else
+         render template: "pages/index"
+      end
    end
 end
